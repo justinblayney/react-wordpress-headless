6f6fa95906f6a9f64ae007b7e7e67cc212b02caf
Justin Blayney <justin@darkstarmedia.net>
2020-12-10 00:25:49 -0500

    got bootstrap menu working

--- src/Components/HeaderComponent/header.js
+++ src/Components/HeaderComponent/header.js
@@ -2,7 +2,10 @@ import React, { Component, useState, useEffect } from 'react';
 import {
 	Link
 } from 'react-router-dom';
-function Nav() {
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
+
+
+function MyNav() {
   const [menus, setMenus] = useState([]);
 
   useEffect(() => {
@@ -15,47 +18,126 @@ function Nav() {
       .then((res) => res.json())
 
       .then((json) => {
-        console.log(json);
+        
         setMenus(json.items);
       });
   }, []);
 
+	
+	
   return (
-    <nav>
-      <ul style={{ display: "flex" }}>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+	  
+		<button 
+		  className="navbar-toggler" 
+		  type="button" 
+		  data-toggle="collapse" 
+		  data-target="#navbarNavDropdown" 
+		  aria-controls="navbarNavDropdown" 
+		  aria-expanded="false" 
+		  aria-label="Toggle navigation">
+			<span className="navbar-toggler-icon"></span>
+	  	</button>
+	  
+	  <div className="collapse navbar-collapse" id="navbarNavDropdown">  
+      <ul className="navbar-nav">
         {menus &&
           menus.map((item) => (
-            <li key={item.ID}>
-              <Link
-                to={{
-                  pathname: item.slug,
-                  state: {
-                    pageId: item.object_id
-                  }
-                }}
-              >
-                {item.title}
-              </Link>
+     	    
+	  			<li 
+	  			className={
+                    item.child_items
+                      ? "nav-item dropdown"
+                      : "nav-item  "
+                  } 
+				key={item.ID}>
+      	       
+	  			<Link
+	  			
+	  
+	  			className={
+                    item.child_items
+                      ? "nav-link dropdown-toggle"
+                      : "nav-link  "
+                  } 
+	  
+	 			 id={
+                    item.child_items
+                      ? "navbarDropdownMenuLink"
+                      : ""
+                  } 
+	  
+	  			data-toggle={
+                    item.child_items
+                      ? "dropdown"
+                      : ""
+                  } 
+	  
+	  			aria-haspopup={
+                    item.child_items
+                      ? "true"
+                      : ""
+                  } 
+	  
+	  			aria-expanded={
+                    item.child_items
+                      ? "false"
+                      : ""
+                  } 
+	  	     	 
+	  				
+	  				 to={{
+					  pathname: item.slug,
+					  state: {
+						pageId: item.slug
+					  }
+					}}
+				  >
+					{item.title}
+				  </Link>
+	  
+	 
+	  		<div 
+	  
+	  			className={
+                    item.child_items
+                      ? "dropdown-menu"
+                      : ""
+                  } 
+	  
+	  			aria-labelledby={
+                    item.child_items
+                      ? "navbarDropdownMenuLink"
+                      : ""
+                  } 
+	  			>
+	 
+	  
               {item.child_items &&
                 item.child_items.map((childItem) => (
-                  <ul>
-                    <li key={childItem.ID}>
+                  
+                  
                       <Link
+	  					key={childItem.ID}
+	  					className="dropdown-item"
                         to={{
                           pathname: childItem.slug,
                           state: {
-                            pageId: childItem.object_id
+                            pageId: childItem.slug
                           }
                         }}
                       >
                         {childItem.title}
                       </Link>
-                    </li>
-                  </ul>
-                ))}
+                    
+                
+  					))}
+
+  				</div>
             </li>
           ))}
       </ul>
+	 </div>
     </nav>
   );
 }
@@ -74,7 +156,7 @@ render() {
 	  	<div className="row">
 			 <div className="col-12">
 	  			<div className="logo">Logo</div>
-				  <Nav />
+				  <MyNav />
 					
 					
 			</div> 
